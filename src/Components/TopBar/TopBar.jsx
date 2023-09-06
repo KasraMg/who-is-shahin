b7bf356f7d54abafdf3506d467974edfe9912ddf
KasraMg <kasragithub7@gmail.com>
2023-09-06 13:21:03 +0330

    topbarFa

--- src/Components/TopBar/TopBar.jsx
+++ src/Components/TopBar/TopBar.jsx
@@ -15,19 +15,19 @@ export default function TopBar() {
 
         <img src="/Images/photo_5847965502901698114_x.jpg" className='me-image-2   sm-x2:w-60  sm-x2:h-64  sm-x2:relative  sm-x2:z-50    hidden md:block object-cover  w-72  h-80   mx-auto mb-4 rounded-full' alt="" />
         <div className='text-9xl md:text-6xl md:text-center   sm-x2:mt-8 sm-x3:!text-4xl  md:w-full w-max relative text-white font-swap font-extrabold'>
-          <span className='z-20 relative'>{t('topbar.title')}</span>
-           <img className='absolute top-4 md:right-40 md:top-0 w-36  z-10 sm-x3:!right-0  right-2 ' src="https://demo.themecon.net/onlyme/html/images/hero-title-pettern-bg.png" alt="" />
+          <span className='z-20 relative sm-x2:whitespace-nowrap'>{t('topbar.title')}</span>
+           <img className={ ` ${context.language=='fa' ? 'md:left-40 sm-x3:!left-0  left-10' : ' md:right-40 sm-x3:!right-0  right-2'} absolute top-4 md:top-0 w-36  z-10   `} src="https://demo.themecon.net/onlyme/html/images/hero-title-pettern-bg.png" alt="" />
            </div>
         <p className='text-5xl md:text-3xl md:text-center md:w-full mt-4 w-max relative  sm-x2:!text-2xl text-white font-swap font-extrabold z-10'> {t('topbar.myJob')}</p>
-        <div className=' pt-12 md:text-center'>
+        <div className={` ${context.language =='fa' ? ' gap-3 flex items-center justify-start sm:justify-center' : null}  pt-12 md:text-center  `}>
           <span className='text-2xl   text-white font-swap font-extrabold'>{t('topbar.hire')}</span>
           <a href="#footer" className='z-[999] relative'><button className='bg-red-600 text-2xl relative top-1 rounded-full ml-3 p-4 text-white'><HiArrowLongRight /></button></a>
         </div>
-        <img className='TopBar-logo absolute    sm-x2:left-0  sm-x2:w-full' src="https://demo.themecon.net/onlyme/html/images/vertical-logo.png" alt="" />
-        <img src="/Images/shomal2.jpg" className='me-image absolute right-[220px] w-[420px] -top-[74px] lg:right-28 md:hidden ' alt="" />
-        <img src="https://demo.themecon.net/onlyme/html/images/dot-texture.png" className='dot-bg md:!right-[45%] md:z-[-1]   lg:right-5 right-[97px]  md:-top-20 top-32 absolute' alt="" />
+        <img className='TopBar-logo absolute sm-x2:left-0  sm-x2:w-full' src="https://demo.themecon.net/onlyme/html/images/vertical-logo.png" alt="" />
+        <img src="/Images/shomal2.jpg" className={`${context.language=='fa' ? 'left-[220px] lg:left-28':'right-[220px] lg:right-28'} me-image absolute   w-[420px] -top-[74px]  md:hidden`}  alt="" />
+        <img src="https://demo.themecon.net/onlyme/html/images/dot-texture.png" className={`${context.language=='fa' ? ' md:!left-[45%]  lg:left-5 left-[97px] ':' md:!right-[45%]  lg:right-5 right-[97px]'} dot-bg  md:z-[-1] md:-top-20 top-32 absolute`} alt="" />
       </section>
-      <div className='flex gap-5 w-11 ml-auto  sm-x2:relative  sm-x2:top-12  pb-10 flex-col text-2xl text-white'>
+      <div className={` ${context.language=='fa' ? 'mr-auto ml-10' : ' ml-auto '} flex gap-5 w-11 sm-x2:relative  sm-x2:top-12  pb-10 flex-col text-2xl text-white` }>
                 <a href="https://instagram.com/_ka.s.ra_?igshid=ZDc4ODBmNjlmNQ==" target='_blank'><AiOutlineInstagram className='ml-auto mr-7 '/></a>
                         <a href="https://t.me/shahiinnnnn" target='_blank'><BiLogoTelegram className='ml-auto mr-7 '/></a>
                         <a href="https://github.com/KasraMg" target='_blank'><BsGithub className='ml-auto mr-7 '/></a>
